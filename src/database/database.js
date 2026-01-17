b5d34caec0d57c2baf4471d44cf977c45e8083bf
Stalonjoyaldsouza <stalonjdsouza24@gmail.com>
2026-01-17 21:49:45 +0530

    database was added

---
+++ src/database/database.js
@@ -0,0 +1,18 @@
+import  {DB_URI,NODE_ENV} from '../../config/env.js';
+import mongoose from 'mongoose';
+if(!DB_URI){
+    throw  new Error('please declare the database uri variable in the .env.local');
+}
+
+const connecttodatabase= async() =>{
+    try{
+        await mongoose.connect(DB_URI);
+        console.log(`CONNECT TO THE DATABASE : ${NODE_ENV}`);
+        
+    }
+    catch(error){
+        console.error('error connecting to database ',error);
+        process.exit(1);
+    }
+}
+export default connecttodatabase ;
